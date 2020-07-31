6c8d11ba3131b6029fe54afb57e3780593036820
Viet Phi Nguyen <vietphi99dh@gmail.com>
2020-07-31 18:14:17 +0700

    complete lab 01

--- lab01/ex14/ex14.cpp
+++ lab01/ex14/ex14.cpp
@@ -1,20 +1,90 @@
-// ex14.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <math.h>
+#include "..\lab01\Dependencies\glew\glew.h"
+#include "..\lab01\Dependencies\freeglut\freeglut.h"
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int screenWidth = 640;
+const int screenHeight = 480;
+const double R = 150;
+const double pi = 3.141592654;
+
+struct GLdoublePoint {
+	GLdouble x;
+	GLdouble y;
+};
+
+void init() {
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glShadeModel(GL_FLAT);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPointSize(5.0);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glBegin(GL_POINTS);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	GLdoublePoint V1, V2, V3, V4, V5, V0, V6, V7, V8, V9, V10;
+	V0.x = screenWidth / 2;
+	V0.y = screenHeight / 2;
+	V1.x = V0.x;
+	V1.y = V0.y + R;
+	V2.x = V0.x + R * sin(2 * pi / 5);
+	V2.y = V0.y + R * cos(2 * pi / 5);
+	V3.x = V0.x + R * sin(pi / 5);
+	V3.y = V0.y - R * cos(pi / 5);
+	V4.x = V0.x - R * sin(pi / 5);
+	V4.y = V0.y - R * cos(pi / 5);
+	V5.x = V0.x - R * sin(2 * pi / 5);
+	V5.y = V0.y + R * cos(2 * pi / 5);
+
+	V6.x = V0.x;
+	V6.y = V0.y - R / 2;
+	V7.x = V0.x - R / 2 * sin(2 * pi / 5);
+	V7.y = V0.y - R / 2 * cos(2 * pi / 5);
+	V8.x = V0.x - R / 2 * sin(pi / 5);
+	V8.y = V0.y + R / 2 * cos(pi / 5);
+	V9.x = V0.x + R / 2 * sin(pi / 5);
+	V9.y = V0.y + R / 2 * cos(pi / 5);
+	V10.x = V0.x + R / 2 * sin(2 * pi / 5);
+	V10.y = V0.y - R / 2 * cos(2 * pi / 5);
+
+	glVertex2d(V1.x, V1.y);
+	glVertex2d(V2.x, V2.y);
+	glVertex2d(V3.x, V3.y);
+	glVertex2d(V4.x, V4.y);
+	glVertex2d(V5.x, V5.y);
+
+	glVertex2d(V6.x, V6.y);
+	glVertex2d(V7.x, V7.y);
+	glVertex2d(V8.x, V8.y);
+	glVertex2d(V9.x, V9.y);
+	glVertex2d(V10.x, V10.y);
+
+	glEnd();
+
+	glFlush();
+}
+
+void reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h);
+}
+
+int main(int agrc, char** argv) {
+	glutInit(&agrc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(screenWidth, screenHeight);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+	return 0;
+}
+
+
+
---
+++ lab01/ex15/ex15.cpp
@@ -0,0 +1,103 @@
+#include <math.h>
+#include "..\lab01\Dependencies\glew\glew.h"
+#include "..\lab01\Dependencies\freeglut\freeglut.h"
+
+
+#define STEPS 40
+#define MAX_STARS 300
+#define PI 3.14159265358979323846
+
+const int screenWidth = 640;
+const int screenHeight = 480;
+
+typedef struct star {
+	GLint x, y;
+	GLint radius;
+	GLint velocity;
+	GLfloat intensity;
+} STAR;
+
+STAR sky[MAX_STARS];
+
+void init(void) {
+	glClearColor(0.0, 0.0, 0.0, 0.0); // black
+}
+
+void glCircle(GLint x, GLint y, GLint radius) {
+	GLfloat twicePi = (GLdouble)2.0f * PI;
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2i(x, y);
+	for (int i = 0; i <= STEPS; i++) {
+		glVertex2i((GLint)(x + (radius * cos(i * twicePi / STEPS)) + 0.5), (GLint)(y + (radius * sin(i * twicePi / STEPS)) + 0.5));
+	}
+	glEnd();
+}
+
+void skyInit() {
+	for (int i = 0; i < MAX_STARS; i++) {
+		sky[i].x = rand() % screenWidth;
+		sky[i].y = rand() % screenHeight;
+		sky[i].radius = 1 + rand() % 3;
+		sky[i].intensity = sky[i].radius / 3.0f;
+		sky[i].velocity = sky[i].radius * 2 + rand() % 3;
+	}
+}
+
+void skyDraw() {
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	for (int i = 0; i < MAX_STARS; i++) {
+		glColor3f(sky[i].intensity, sky[i].intensity, sky[i].intensity);
+		glCircle(sky[i].x, sky[i].y, sky[i].radius);
+	}
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+
+	glPushMatrix();
+	skyDraw();
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void update() {
+	for (int i = 0; i < MAX_STARS; i++) {
+		sky[i].x += sky[i].velocity;
+		if (sky[i].x >= screenWidth) {
+			sky[i].x = 0;
+			sky[i].y = rand() % screenHeight;
+			sky[i].radius = 1 + rand() % 3;
+			sky[i].intensity = sky[i].radius / 3.0f;
+			sky[i].velocity = sky[i].radius * 2 + rand() % 3;
+		}
+	}
+
+	Sleep(50);
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D((GLdouble)w, 0.0, 0.0, (GLdouble)h);
+}
+
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(screenWidth, screenHeight);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	skyInit();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(update);
+	glutMainLoop();
+	return 0;
+}
